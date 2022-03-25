6e27d1076038425f5c45db683723eca882d403d6
onshore <onshorecoding9@gmail.com>
2022-03-25 18:55:59 +0900

    Feat: Next, TypeScript 기본 설정

---
+++ src/containers/main/ProductGrid/ProductGrid.styled.ts
@@ -0,0 +1,19 @@
+import styled from 'styled-components';
+
+export const HomeBlock = styled.div`
+  min-height: 100vh;
+  padding: 0 0.5rem;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const Box = styled.div`
+  min-height: 100vh;
+  padding: 0 0.5rem;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
---
+++ src/containers/main/ProductGrid/ProductGrid.tsx
@@ -0,0 +1,7 @@
+import { HomeBlock } from "./ProductGrid.styled";
+
+const ProductGrid = () => {
+  return <HomeBlock>hello</HomeBlock>;
+};
+
+export default ProductGrid;
